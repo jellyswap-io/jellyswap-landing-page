d470c03cb2a26aa409826d901c4ec3da7ef0f324
Nick <nicholashulston@gmail.com>
2021-07-02 14:17:26 -0700

    whitepaper page

---
+++ packages/landing/src/containers/Crypto/WhitepaperBanner/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from 'common/components/Box';
+import Heading from 'common/components/Heading';
+import FeatureBlock from 'common/components/FeatureBlock';
+import Container from 'common/components/UI/Container';
+import WhitepaperBannerWrapper from "./whitepaperBanner.style";
+
+const WhitepaperBannerSection = ({
+  row,
+  col,
+  title,
+}) => {
+  return (
+    <WhitepaperBannerWrapper id="whitepaper_banner_section">
+      <Container>
+        <Box className="row" {...row}>
+          <Box className="col" {...col}>
+            <FeatureBlock
+              title={<Heading content="Whitepaper" {...title} />}
+            />
+            <FeatureBlock/>
+          </Box>
+        </Box>
+      </Container>
+    </WhitepaperBannerWrapper>
+  );
+};
+
+WhitepaperBannerSection.propTypes = {
+  title: PropTypes.object,
+  btnStyle: PropTypes.object,
+  description: PropTypes.object,
+  contentStyle: PropTypes.object,
+  discountText: PropTypes.object,
+  discountAmount: PropTypes.object,
+  outlineBtnStyle: PropTypes.object,
+};
+
+WhitepaperBannerSection.defaultProps = {
+  row: {
+    flexBox: true,
+    flexWrap: 'wrap',
+    ml: '-15px',
+    mr: '-15px',
+    alignItems: 'center',
+  },
+  col: {
+    pr: '15px',
+    pl: '15px',
+    width: ['100%', '100%', '100%', '60%', '65%'],
+  },
+  title: {
+    fontSize: ['52px', '68px', '84px', '84px', '94px'],
+    fontWeight: '700',
+    color: '#fff',
+    letterSpacing: '-0.025em',
+    mb: ['20px', '25px'],
+    lineHeight: '1.31',
+  },
+  description: {
+    fontSize: ['14px', '16px', '18px', '18px', '20px'],
+    color: '#fff',
+    lineHeight: '30px',
+    mb: '0',
+    maxWidth: '550px',
+  },
+  btnStyle: {
+    minWidth: ['120px', '156px'],
+    fontSize: '15px',
+    fontWeight: '700',
+    borderRadius: '6px',
+  },
+  outlineBtnStyle: {
+    minWidth: ['130px', '156px'],
+    fontSize: '16px',
+    fontWeight: '700',
+    color: '#fff',
+    p: '5px 10px',
+  },
+};
+
+export default WhitepaperBannerSection;
---
+++ packages/landing/src/pages/whitepaper.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+import Sticky from 'react-stickynode';
+import { ThemeProvider } from 'styled-components';
+import { cryptoTheme } from 'common/theme/crypto';
+import { ResetCSS } from 'common/assets/css/style';
+import { GlobalStyle, ContentWrapper } from 'containers/Crypto/crypto.style';
+import { DrawerProvider } from 'common/contexts/DrawerContext';
+import Navbar from 'containers/Crypto/Navbar';
+import Banner from 'containers/Crypto/BannerSection';
+import BannerSlider from 'containers/Crypto/BannerSlider';
+import Dex from 'containers/Crypto/Dex';
+import ControlSections from 'containers/Crypto/ControlSection';
+import TrustedProofSections from 'containers/Crypto/TrustedProof';
+import ScalableSections from 'containers/Crypto/ScalableSection';
+import SlideSections from 'containers/Crypto/CryptoSlides';
+import ValueSections from 'containers/Crypto/ValueSection';
+import Footer from 'containers/Crypto/Footer';
+import SocialSection from "../containers/Crypto/SocialSection";
+import WhitepaperBanner from "../containers/Crypto/WhitepaperBanner";
+import WhitepaperSection from "../containers/Crypto/Whitepaper";
+
+const WhitepaperPage = () => {
+  return (
+    <ThemeProvider theme={cryptoTheme}>
+      <Fragment>
+        <Head>
+          <title>JellySwap | Cardano DEX</title>
+          <meta name="Description" content="React next landing page" />
+          <meta name="theme-color" content="#ec5555" />
+
+          {/* Load google fonts */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Heebo:300,400,500,700"
+            rel="stylesheet"
+          />
+        </Head>
+        <ResetCSS />
+        <GlobalStyle />
+        <ContentWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <WhitepaperBanner />
+          <WhitepaperSection />
+          <Footer />
+        </ContentWrapper>
+      </Fragment>
+    </ThemeProvider>
+  );
+};
+export default WhitepaperPage;
