64fa77b0e00ff69f19041613e3161d992d1150ba
Joseph Vitko <josephvitko21@gmail.com>
2021-07-02 19:05:42 -0400

    whitepaper styling

--- packages/landing/src/containers/Crypto/Whitepaper/index.js
+++ packages/landing/src/containers/Crypto/Whitepaper/index.js
@@ -28,19 +28,29 @@ const WhitepaperSection = ({
           className="section-heading"
           content={'Abstract'}
         />
-        <Text
-          {...text}
-          content="With Smart Contracts being released on the Cardano network later this year, there is a great need for a market to trade the new currencies that will pop up. This whitepaper describes JellySwap, a decentralized exchange (DEX) built on the Cardano blockchain. It will be modeled after the popular Uniswap on the Ethereum blockchain but will offer more attractive fees due to Cardano’s greater efficiency. Anyone can serve as a liquidity provider on JellySwap, creating a market for others to exchange their Cardano tokens. Traders pay a small fee of 0.25%, giving liquidity providers profit in return for providing liquidity."
-        />
+        <p style={text}>
+          <span className="indent" />
+          With Smart Contracts being released on the Cardano network later this
+          year, there is a great need for a market to trade the new currencies
+          that will pop up. This whitepaper describes JellySwap, a decentralized
+          exchange (DEX) built on the Cardano blockchain. It will be modeled
+          after the popular Uniswap on the Ethereum blockchain but will offer
+          more attractive fees due to Cardano’s greater efficiency. Anyone can
+          serve as a liquidity provider on JellySwap, creating a market for
+          others to exchange their Cardano tokens. Traders pay a small fee of
+          0.25%, giving liquidity providers profit in return for providing
+          liquidity."
+        </p>
         <Text {...sectionTitle} content={'Background'} />
         <p style={text}>
-          JellySwap is a “Decentralized Exchange” (DEX), an application running
-          on smart contracts on the Cardano Blockchain. Rather than relying on a
-          central entity such as a bank, JellySwap aims to create a free
-          financial market relying on these smart contracts to manage
-          transactions.
+          <span className="indent" /> JellySwap is a “Decentralized Exchange”
+          (DEX), an application running on smart contracts on the Cardano
+          Blockchain. Rather than relying on a central entity such as a bank,
+          JellySwap aims to create a free financial market relying on these
+          smart contracts to manage transactions.
           <br />
           <br />
+          <span className="indent" />
           Without a central authority determining the price of coins, a DEX
           relies on liquidity providers to create a market. Thus, the price of
           coins are mathematically determined based on the quantity of each coin
@@ -50,6 +60,7 @@ const WhitepaperSection = ({
           create a stronger, more liquid market.
           <br />
           <br />
+          <span className="indent" />
           Several DEXs have been built on existing blockchains such as Uniswap
           on Ethereum and PancakeSwap on the Binance Smart Chain. However,
           JellySwap aims to be the primary DEX of choice on the Cardano
@@ -61,15 +72,17 @@ const WhitepaperSection = ({
           mentioned coins have their own fatal flaws:
           <br />
           <br />
-          • Bitcoin does not have Smart Contracts and uses Proof of Work
+          <p style={{ 'margin-left': '40px' }}>
+            • Bitcoin does not have Smart Contracts and uses Proof of Work
+            <br />
+            <br />
+            • Ethereum uses Proof of Work
+            <br />
+            <br />• Binance is centralized
+          </p>
           <br />
           <br />
-          • Ethereum uses Proof of Work
-          <br />
-          <br />
-          • Binance is centralized
-          <br />
-          <br />
+          <span className="indent" />
           In contrast, the Cardano blockchain was created using proof of stake
           with a focus on energy efficiency. With a network that is 1.6 million
           times more energy efficient than Bitcoin, Cardano has significantly
@@ -87,6 +100,7 @@ const WhitepaperSection = ({
           content={'Efficiency: PoW vs PoS'}
         />
         <p style={text}>
+          <span className="indent" />
           Ethereum and Bitcoin run using Proof of Work, which requires huge
           amounts of energy leading to expensive mining costs, slow
           transactions, and high transaction fees. It is bad for the environment
@@ -98,6 +112,7 @@ const WhitepaperSection = ({
           pools,” leading to a more centralized blockchain
           <br />
           <br />
+          <span className="indent" />
           Cardano runs using Proof of Stake where “validators” are randomly
           chosen based on the amount of stake (similar to a security deposit)
           they provide. They validate the transaction in return for a fee.
@@ -115,6 +130,7 @@ const WhitepaperSection = ({
           content={'Liquidity Pools'}
         />
         <p style={text}>
+          <span className="indent" />
           Traditional financial markets rely on a central authority to act as
           the market maker, matching buyers and sellers together. However, this
           entity has the potential to manipulate the market, resulting in the
@@ -123,6 +139,7 @@ const WhitepaperSection = ({
           the price of two tokens relative to each other, using liquidity pools.
           <br />
           <br />
+          <span className="indent" />
           To understand how liquidity pools work, suppose a BTC/ETH liquidity
           pool has 9 BTC and 90 ETH and has issued a total of 9 tracking tokens
           to liquidity providers. For every tracking token a liquidity provider
@@ -133,6 +150,7 @@ const WhitepaperSection = ({
           <Image src={Figure1} alt="Example user and liquidity pool" />
         </div>
         <p style={text}>
+          <span className="indent" />
           If a user now deposits 1 BTC and 10 ETH into the liquidity pool, they
           will be issued 1 tracking token out of a total of 10 tracking tokens
           in the entire liquidity pool.
@@ -142,6 +160,7 @@ const WhitepaperSection = ({
           <Image src={Figure2} alt={'Depositing liquidity'} />
         </div>
         <p style={text}>
+          <span className="indent" />
           This pool also allows swaps to happen. For example, a trader can swap
           BTC for ETH. If they swap 0.1 BTC for 1 ETH, the pool now has 10.1 BTC
           and 99 ETH. However, the number of tracking tokens, 10, does not
@@ -156,6 +175,7 @@ const WhitepaperSection = ({
           <Image src={Figure3} alt="Example of swapping 0.1 BTC for 1 ETH" />
         </div>
         <p style={text}>
+          <span className="indent" />
           However, this linear price calculation opens the market up for abuse.
           If enough BTC were traded for ETH such that there was hardly any
           ethereum left in the liquidity pool, the price of ETH would be near
@@ -167,6 +187,7 @@ const WhitepaperSection = ({
           <Image src={Equation1} alt={'Equation1'} />
         </div>
         <p style={text}>
+          <span className="indent" />
           By depositing 0.1 BTC, the total balance of BTC would become 10.1, and
           we can calculate the amount of ETH to withdraw to balance the
           equation. The amount of ETH left in the pool after the swap must be:
@@ -179,6 +200,7 @@ const WhitepaperSection = ({
           <Image src={Equation3} alt={'Equation3'} />
         </div>
         <p style={text}>
+          <span className="indent" />
           Since the expected ETH for .1 BTC was 1 ETH but the user only got 0.99
           ETH, there was a “price slippage” of 1%. User B got 99% of the
           expected value at the current price. However, as the size of the
@@ -186,6 +208,7 @@ const WhitepaperSection = ({
           slippage.
           <br />
           <br />
+          <span className="indent" />
           Now suppose a user tries to swap 5 BTC for ETH in a pool that
           originally has 10 BTC and 100 ETH. The expected quantity of ETH
           received is 50 ETH. However, since the swap makes up such a large
@@ -196,6 +219,7 @@ const WhitepaperSection = ({
           <Image src={Equation4} alt={'Equation4'} />
         </div>
         <p style={text}>
+          <span className="indent" />
           Since the expected ETH was 50 but the user only got 33, there was
           33/50 = 66% efficiency, or a price slippage of 33%. Thus, it is
           beneficial to encourage as much liquidity as possible to give swappers
@@ -210,21 +234,24 @@ const WhitepaperSection = ({
           content={'Bringing Value to Jelly Token'}
         />
         <p style={text}>
+          <span className="indent" />
           We will take several measures to capture value for the $JELLY token.
           Each transaction will have a fee of 0.25%:
           <br />
           <br />
-          • 0.17% transaction fee returned to liquidity providers to encourage
-          people to provide more liquidity. This will also bring new investors
-          to $JELLY, increasing demand
-          <br />
-          <br />
-          • 0.05% transaction fee used to buy back and burn $JELLY tokens,
-          creating a stable source of demand while reducing the supply of $JELLY
-          on the market
-          <br />
-          <br />• 0.03% transaction fee for the $JELLY treasury, used to build
-          infrastructure and develop the project further
+          <p style={{ 'margin-left': '40px' }}>
+            • 0.17% transaction fee returned to liquidity providers to encourage
+            people to provide more liquidity. This will also bring new investors
+            to $JELLY, increasing demand
+            <br />
+            <br />
+            • 0.05% transaction fee used to buy back and burn $JELLY tokens,
+            creating a stable source of demand while reducing the supply of
+            $JELLY on the market
+            <br />
+            <br />• 0.03% transaction fee for the $JELLY treasury, used to build
+            infrastructure and develop the project further
+          </p>
         </p>
 
         <Text
@@ -233,12 +260,14 @@ const WhitepaperSection = ({
           content={'Conclusion'}
         />
         <p style={text}>
+          <span className="indent" />
           The Cardano blockchain has lower fees, faster transaction times, a
           higher transaction/second limit, and greater energy efficiency. With
           worldwide adoption of cryptocurrencies in motion, Cardano could likely
           be the cryptocurrency of choice for businesses and individuals alike.
           <br />
           <br />
+          <span className="indent" />
           With Smart Contracts launching later this year, there will be a huge
           surge in economic activity on the Cardano blockchain, and JellySwap
           aims to fulfill the need of a market to trade these tokens.
